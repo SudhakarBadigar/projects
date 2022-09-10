658a9fbd9ec4675a84b5dd40890a26451733ef85
SudhakarBadigar <sudhakar.badigar87@gmail.com>
2022-09-10 18:15:21 +0530

    projects in Process

---
+++ scrimba/countApp/index.js
@@ -0,0 +1,18 @@
+// //document.getElementById("count-el").innerText=10;
+
+// // intialize the count as 0
+// let count = 0;
+// // console.log(count);
+// // listen for clicks on the increment button
+// function incr(){
+//     increment-btn;
+// }
+// // increment the count variable when the button is clicked
+// count = count + increment-btn;
+// // change the count-el in the HTML to reflect the new count
+let count = 0;
+function incr(){
+    count = count + 1;
+    console.log(count);
+}
+
---
+++ scrimba/countApp/practice.js
@@ -0,0 +1,8 @@
+let bonusPoints = 50;
+console.log("bonuspoint :" + bonusPoints);
+bonusPoints = bonusPoints + 50;
+console.log ("bonusPoints :" + bonusPoints);
+bonusPoints = bonusPoints - 75;
+console.log("bonusPoints :"  + bonusPoints);
+bonusPoints= bonusPoints + 45;
+console.log("bonusPoints :"  + bonusPoints);
